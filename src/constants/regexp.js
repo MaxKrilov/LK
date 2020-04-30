a6e965cd0c23dac3db149197744cd7d5a9ee3d90
Лутченко Артем Андреевич <artem.lutchenko@domru.ru>
2020-04-30 17:17:21 +0500

    [web-fix-email-regexp] Фикс регулярки для проверки E-mail (добавление кириллических символов)

--- src/constants/regexp.js
+++ src/constants/regexp.js
@@ -4,7 +4,7 @@ export const PATTERN_NUMBERS = /[0-9]/
 export const PATTERN_SYMBOLS = /[_\]\[\-.!#]/
 export const PATTERN_BAD_SYMBOLS = /[,;:/\\|=+*'"`~@№$%^&?<>(){}éèêëùüàâöïç\s]/
 export const PATTERN_PASSWORD = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(^[0-9a-zA-Z\[_\-.!#\]]{8,16}$)/
-export const PATTERN_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+export const PATTERN_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-ZА-Яа-я\-0-9]+\.)+[a-zA-ZА-Яа-я]{2,}))$/
 export const PATTERN_NOT_LETTERS = /^[a-zA-Z]+$/
 export const PATTERN_RUSSIAN_PHONE = /^((\s|8|\+7)(-?|\s?))?\(?\d{3}\)?(-?|\s?)?\d{1}(-?|\s?)??\d{1}(-?|\s?)??\d{1}(-?|\s?)??\d{1}(-?|\s?)??\d{1}(-?|\s?)??\d{1}(-?|\s?)??\d{1}/
 export const PATTERN_CYRILLIC_LETTERS = /^[аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩъЪыЫьЬэЭюЮяЯ]+$/
