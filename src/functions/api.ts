1e94dfdde40e64a081baf2fb40261fd251a673ec
Артем Андреевич <artem.lutchenko@domru.ru>
2020-11-10 13:40:35 +0500

    [master]

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -6,7 +6,7 @@ import { BACKEND_COMBAT, BACKEND_STAGING, BACKEND_TESTING } from '@/constants/ur
 import { API_DADATA } from '@/store/actions/api'
 import store from '../store'
 
-const BASE_BRANCH = 'psi1'
+const BASE_BRANCH = 'master'
 
 export class API {
   private _branch = BASE_BRANCH
