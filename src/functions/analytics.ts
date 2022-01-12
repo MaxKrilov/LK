08a92319608cea2078fc72a3c1acf86869c05b36
Artem Lutchenko <artemka.lutchenko@gmail.com>
2022-01-12 16:09:22 +0500

    [master]

--- src/functions/analytics.ts
+++ src/functions/analytics.ts
@@ -6,10 +6,12 @@ export function dataLayerPush (payload: {
 }) {
   // const event = payload.event || 'GA'
   const action = payload.action || 'click'
-  window.dataLayer = window.dataLayer || [];
-  (window as any).gtag('event', action, {
-    event_category: payload.category,
-    event_label: payload.label,
-    value: '1'
-  })
+  window.dataLayer = window.dataLayer || []
+  if ('gtag' in window) {
+    (window as any).gtag('event', action, {
+      event_category: payload.category,
+      event_label: payload.label,
+      value: '1'
+    })
+  }
 }
